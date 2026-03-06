fcc5510cd8358ed09d6fcc34d4dd6841119e7217
Colleen <crusting@horizon.csueastbay.edu>
2026-03-05 16:03:44 -0800

    push

--- TreeT.cpp
+++ TreeT.cpp
@@ -1,1 +1,80 @@
+#include "TreeT.h"
 
+template <class T>
+TreeT<T>::TreeT()
+{
+    root = nullptr;
+    numNodes = 0;
+}
+
+template <class T>
+TreeT<T>::~TreeT()
+{
+}
+
+template <class T>
+TreeT<T>& TreeT<T>::operator=(const TreeT& otherTree)
+{
+}
+
+template <class T>
+void TreeT<T>::Add(T value)
+{
+    Node* parent = nullptr;
+    Node* currN = root;
+
+    while (currN != nullptr)
+    {
+        if (value < currN->value)
+        {
+            parent = currN;
+            currN = currN->left;
+        }
+        else if (value > currN)
+        {
+            parent = currN;
+            currN = currN->right;
+        }
+        else
+        {
+            return;
+        }
+    }
+    Node* newNode = new Node;
+    newNode->value = value;
+    if (value > parent->value)
+    {
+        parent->right = newNode;
+    }
+    else
+    {
+        parent->left = newNode;
+    }
+    numNodes++;
+}
+
+template <class T>
+void TreeT<T>::Remove(T value)
+{
+}
+
+template <class T>
+bool TreeT<T>::Contains(T value)
+{
+}
+
+template <class T>
+int TreeT<T>::Size()
+{
+    return numNodes;
+}
+
+template <class T>
+void TreeT<T>::ResetIterator(Order traverseOrder)
+{
+}
+
+template <class T>
+T TreeT<T>::GetNextItem()
+{
+}
